7a7f1b0730c3b1b279899d04a576e453190c60a5
Ivan López <ivan.lopez1@unmsm.edu.pe>
2022-02-12 20:00:15 -0500

    agregar metodos en users.router

--- routes/products.router.js
+++ routes/products.router.js
@@ -23,16 +23,23 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  });
+  if(id==='999'){
+    res.status(404).json({
+      message:'Not found'
+    })
+  }
+  else{
+    res.json({
+      id,
+      name: 'Product 2',
+      price: 2000
+    });
+  }
 });
 
 router.post('/', (req,res)=>{
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,32 +1,60 @@
 const express = require('express');
-
+const faker = require('faker');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  //const limit = req.query.limit;
-  //const offser = req.query.offser;
-  if(limit && offset){
-    res.json({
-      limit,
-      offset
-    })
+  const users = [];
+  const{size} = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    users.push({
+      name: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      image: faker.image.imageUrl(),
+    });
   }
-  else{
-    res.send('No hay parametros')
-  }
-
+  res.json(users);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
  res.json({
    id,
-   name:'Ivan',
-   age:23,
-   role:'Desarrollador web'
+   name: faker.name.firstName(),
+   lastName: faker.name.lastName(),
+   image: faker.image.imageUrl(),
  })
 
 });
+
+router.post('/', (req,res)=>{
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body
+  })
+});
+
+router.patch('/:id', (req,res)=>{
+  const {id} = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Update',
+    data: body,
+    id
+  })
+});
+
+
+router.delete('/:id', (req,res)=>{
+  const {id} = req.params;
+  res.json({
+    message: 'Delete',
+    id
+  })
+});
+
+
+
 
 module.exports = router;
